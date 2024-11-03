ef9da8a920e8accf4d00cdfe2b7c1b5f8e21a8dd
Rodney James <124419269+Veritaaaas@users.noreply.github.com>
2024-11-03 09:41:06 +0000

    complete register form  component

---
+++ src/components/auth/register-form.tsx
@@ -0,0 +1,103 @@
+'use client'
+import CardWrapper from "./card-wrapper"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from 'react-hook-form'
+import { registerSchema } from "../../../schema"
+import { z } from "zod"
+
+export default function RegisterForm(): JSX.Element {
+    const form = useForm({
+        resolver: zodResolver(registerSchema),
+        defaultValues: {
+            email: "",
+            username: "",
+            password: "",
+            confirmPassword: "",
+        },
+    })
+
+    const onSubmit = (values: z.infer<typeof registerSchema>): void => {
+        console.log(values)
+    }
+
+    return (
+        <CardWrapper 
+            label="Register"
+            title="Create an account"
+            backButtonText="Already have an account? Login here."
+            backButtonPath="/auth/login"
+        >
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                    <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Email</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="email@example.com" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="username"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Username</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="johndoe" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input type="password" {...field} placeholder="******"/>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="confirmPassword"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Confirm Password</FormLabel>
+                                <FormControl>
+                                    <Input type="password" {...field} placeholder="******"/>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit" className="w-full">
+                        Register
+                    </Button>
+                </form>
+            </Form>
+        </CardWrapper>
+    )
+}
