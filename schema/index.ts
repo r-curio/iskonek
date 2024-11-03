47c28ae065fe3080a2b717720856119264684149
Rodney James <124419269+Veritaaaas@users.noreply.github.com>
2024-11-03 07:06:13 +0000

    Add registration schema validation using Zod

---
+++ schema/index.ts
@@ -0,0 +1,16 @@
+import * as z from 'zod';
+
+export const registerSchema = z.object({
+    email: z.string().email({
+        message: "Enter a valid email address"
+    }),
+    username: z.string().min(3, {
+        message: "Username must be at least 3 characters long"
+    }),
+    password: z.string().min(6, {
+        message: "Password must be at least 6 characters long"
+    }),
+    confirmPassword: z.string().min(6, {
+        message: "Password must be at least 6 characters long"
+    }),
+});
