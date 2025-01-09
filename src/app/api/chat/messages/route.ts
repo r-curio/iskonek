9487e320f423392abb5adcb21e234753e7300dfb
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2025-01-09 10:52:56 +0800

    feat: create route handler for sending messages

---
+++ src/app/api/chat/messages/route.ts
@@ -0,0 +1,31 @@
+import { createClient } from '@/utils/supabase/server'
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {
+    const supabase = await createClient()
+    
+    try {
+        const { content, roomId } = await request.json()
+        const { data: { user }, error: userError } = await supabase.auth.getUser()
+        
+        if (!user || userError) {
+            return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
+        }
+
+        const { error } = await supabase
+            .from('messages')
+            .insert({
+                content,
+                room_id: roomId,
+                sender_id: user.id
+            })
+
+        if (error) {
+            return NextResponse.json({ error: 'Failed to send message' }, { status: 500 })
+        }
+
+        return NextResponse.json({ status: 'success' })
+    } catch (error) {
+        return NextResponse.json({ error }, { status: 500 })
+    }
+}
