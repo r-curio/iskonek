deae2425de3bd9cc4498c41018ec8ab963d261ba
znarfm <44516102+znarfm@users.noreply.github.com>
2025-01-10 11:41:40 +0800

    refactor: improve code formatting and readability

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 
 interface ChatHeaderProps {
-    recipientName: string | undefined
-    recipientProfilePic?: string;
+  recipientName: string | undefined;
+  recipientProfilePic?: string;
 }
 
-const ChatHeader: React.FC<ChatHeaderProps> = ({ recipientName, recipientProfilePic }) => {
-    return (
-        <div className="flex items-center p-4 border-b max-h-16  shadow-lg">
-            <Avatar className="mr-4">
-                {recipientProfilePic ? (
-                    <AvatarImage src={recipientProfilePic} alt={recipientName} />
-                ) : (
-                    <AvatarFallback>{recipientName?.charAt(0).toUpperCase()}</AvatarFallback>
-                )}
-            </Avatar>
-            <h2 className="text-lg font-semibold">{recipientName}</h2>
-        </div>
-    );
+const ChatHeader: React.FC<ChatHeaderProps> = ({
+  recipientName,
+  recipientProfilePic,
+}) => {
+  return (
+    <div className="flex items-center p-4 border-b max-h-16  shadow-lg">
+      <Avatar className="mr-4">
+        {recipientProfilePic ? (
+          <AvatarImage src={recipientProfilePic} alt={recipientName} />
+        ) : (
+          <AvatarFallback>
+            {recipientName?.charAt(0).toUpperCase()}
+          </AvatarFallback>
+        )}
+      </Avatar>
+      <h2 className="text-lg font-semibold">{recipientName}</h2>
+    </div>
+  );
 };
 
 export default ChatHeader;
