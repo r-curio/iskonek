026e08bc902f408a925eb3dd18e19f7b44996c3a
znarfm <44516102+znarfm@users.noreply.github.com>
2025-06-20 11:52:51 +0800

    feat: implement to login and register forms

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -10,6 +10,7 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { PasswordInput } from "@/components/ui/password-input";
 
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -86,7 +87,7 @@ export default function LoginForm(): JSX.Element {
                   Forgot Password?
                 </Link>
                 <FormControl>
-                  <Input type="password" placeholder="******" {...field} />
+                  <PasswordInput placeholder="******" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
